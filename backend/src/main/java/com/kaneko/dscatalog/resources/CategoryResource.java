887f481441a662e9c3c06199348e75274cee806d
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-09-18 12:40:09 -0300

    criando CategoryResource

--- backend/src/main/java/com/kaneko/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/kaneko/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,26 @@
 package com.kaneko.dscatalog.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kaneko.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findByAll(){
+		List<Category> listCategory = new ArrayList<>();
+		listCategory.add(new Category(1L, "Books"));
+		listCategory.add(new Category(2L, "Eletronics"));
+		
+		return ResponseEntity.ok().body(listCategory);
+	}
 
 }
