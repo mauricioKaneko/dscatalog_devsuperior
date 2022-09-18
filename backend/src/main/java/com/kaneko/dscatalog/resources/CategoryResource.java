45efa46b4f5b8411cad38fb07dd6aa82732b8c24
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-09-18 18:29:45 -0300

    Criando exception

--- backend/src/main/java/com/kaneko/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/kaneko/dscatalog/resources/CategoryResource.java
@@ -1,30 +1,36 @@
 package com.kaneko.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaneko.dscatalog.dto.CategoryDTO;
-import com.kaneko.dscatalog.entities.Category;
 import com.kaneko.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findByAll(){
+	public ResponseEntity<List<CategoryDTO>> findByAll() {
 		List<CategoryDTO> listCategory = categoryService.findAll();
-		
+
 		return ResponseEntity.ok().body(listCategory);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO categoryDto = categoryService.findById(id);
+
+		return ResponseEntity.ok().body(categoryDto);
 	}
 
 }
--- backend/src/main/java/com/kaneko/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/kaneko/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.kaneko.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.kaneko.dscatalog.dto.CategoryDTO;
 import com.kaneko.dscatalog.entities.Category;
 import com.kaneko.dscatalog.repositories.CategoryRepository;
+import com.kaneko.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +24,13 @@ public class CategoryService {
 		List<Category> list = categoryRepository.findAll();
 		
 		return list.stream().map(x ->  new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category category = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));		
+		
+		return new CategoryDTO(category);
 	}
 }
